269cc88a9671b3f4e2ba4896b36471c83be25fa0
charanreddy001 <charanreddypotham111@gmail.com>
2024-03-23 13:31:40 +0530

    c lang program

---
+++ bigg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+    int a,b,c;
+    scanf("%d %d %d",a,b,c);
+    if(a>b){
+        if(a>c){
+            printf("a is big");
+        }
+        else{
+            printf(" c is big");
+        }
+    }
+    else if (b>c){
+        printf("b is big");
+    }
+    else{
+        printf("c is big");
+    }
+}
